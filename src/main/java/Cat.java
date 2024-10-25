6049993147e7212b7af399feaab32408bc986572
Evilasivle <evilasivle@gmail.com>
2024-10-25 18:47:20 +0300

    Добавили POJO класс Cat

---
+++ src/main/java/Cat.java
@@ -0,0 +1,45 @@
+import java.util.Objects;
+
+public class Cat {
+    private String name;
+    private int age;
+
+    public Cat(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cat cat = (Cat) o;
+        return getAge() == cat.getAge() && Objects.equals(getName(), cat.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getAge());
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
